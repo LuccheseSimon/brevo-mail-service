752050dae1e300480d0c305437cb669a187c483e
Simon <57671190+LuccheseSimon@users.noreply.github.com>
2024-07-09 18:06:55 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'The Nubios custom mail service is available';
+    return 'The custom mail service is available';
   }
 }
